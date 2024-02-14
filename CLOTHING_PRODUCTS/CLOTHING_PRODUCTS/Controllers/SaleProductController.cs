140a5ff477b80f0d432f77161490000893f667f1
Akmaral <dzhanyshbekova@gmail.com>
2024-02-14 09:42:38 +0600

    SaleProductControllerAndView

--- CLOTHING_PRODUCTS/CLOTHING_PRODUCTS/Controllers/SaleProductController.cs
+++ CLOTHING_PRODUCTS/CLOTHING_PRODUCTS/Controllers/SaleProductController.cs
@@ -1,6 +1,7 @@
 ﻿using CLOTHING_PRODUCTS.Context;
 using CLOTHING_PRODUCTS.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace CLOTHING_PRODUCTS.Controllers
@@ -22,13 +23,71 @@ namespace CLOTHING_PRODUCTS.Controllers
 
             return View(saleProducts);
         }
-
         public IActionResult Create()
         {
-            ViewBag.FinishedProducts = _dbContext.FinishedProducts.ToList();
-            ViewBag.Employees = _dbContext.Employees.ToList();
+            ViewBag.FinishedProducts = new SelectList(_dbContext.FinishedProducts, "FinishedProductId", "Name");
+            ViewBag.Employees = new SelectList(_dbContext.Employees, "EmployeeId", "FullName");
             return View();
         }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("FinishedProductID, Quantity, Amount, Date, EmployeeID")] SaleProduct saleProduct)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _dbContext.Add(saleProduct);
+        //        await _dbContext.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+
+        //    ViewBag.FinishedProducts = new SelectList(_dbContext.FinishedProducts, "FinishedProductId", "Name", saleProduct.FinishedProductID);
+        //    ViewBag.Employees = new SelectList(_dbContext.Employees, "EmployeeId", "FullName", saleProduct.EmployeeID);
+        //    return View(saleProduct);
+        //}
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FinishedProductID, Quantity, Date, EmployeeID")] SaleProduct saleProduct)
+        {
+          
+            
+            var finishedProduct = await _dbContext.FinishedProducts.FindAsync(saleProduct.FinishedProductID);
+
+            
+            if (finishedProduct.Quantity < saleProduct.Quantity)
+            {
+                ViewData["InsufficientQuantityMessage"] = "Недостаточно товара на складе.";
+                ViewBag.FinishedProducts = new SelectList(_dbContext.FinishedProducts, "FinishedProductId", "Name");
+                ViewBag.Employees = new SelectList(_dbContext.Employees, "EmployeeId", "FullName", saleProduct.EmployeeID);
+                return View(saleProduct);
+            }
+
+            
+            finishedProduct.Quantity -= saleProduct.Quantity;
+
+            var budget = await _dbContext.Budgets.FirstOrDefaultAsync();
+
+            
+            var saleAmount = saleProduct.Quantity * (finishedProduct.Amount / finishedProduct.Quantity) *
+                                ((budget.Percent) / 100);
+
+            
+            budget.BudgetAmount += saleAmount;
+            saleProduct.Amount = saleAmount;
+            finishedProduct.Amount -= saleProduct.Amount;
+            
+            _dbContext.Add(saleProduct);
+
+            
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+          
+        }
+
+
 
 
     }
