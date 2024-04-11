c4066cb6307f003535ab77081f4e16fd835177a2
Akmaral <dzhanyshbekova@gmail.com>
2024-04-12 02:19:28 +0600

    kursovaya_credits

--- CLOTHING_PRODUCTS/CLOTHING_PRODUCTS/Controllers/KURSOVAYACreditController.cs
+++ CLOTHING_PRODUCTS/CLOTHING_PRODUCTS/Controllers/KURSOVAYACreditController.cs
@@ -24,5 +24,43 @@ namespace CLOTHING_PRODUCTS.Controllers
             // Передача списка кредитов в представление для отображения
             return View(credits);
         }
+
+        public IActionResult Create()
+        {
+            // Установка сегодняшней даты по умолчанию для DataOfReceive
+            var defaultDate = DateTime.Today;
+
+            // Создаем новый объект Credit для передачи в представление
+            var credit = new Credit
+            {
+                DataOfReceive = defaultDate
+            };
+
+            return View(credit);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Credit credit)
+        {
+            try
+            {
+                // Вызываем хранимую процедуру для создания кредита
+                _context.Database.ExecuteSqlInterpolated($"EXEC dbo.CreateCredit {credit.SumCredit}, {credit.DataOfReceive}, {credit.Months}, {credit.Perst}, {credit.Penny}");
+
+                // Редирект на страницу списка кредитов после успешного создания
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                // Обработка ошибки создания кредита
+                ModelState.AddModelError(string.Empty, "Ошибка при создании кредита: " + ex.Message);
+                // Если модель недопустима, возвращаем представление с моделью для исправления ошибок
+                return View(credit);
+            }
+            
+
+            
+        }
+
     }
 }
